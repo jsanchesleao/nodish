356d4c910bde37c8778a9c9a4f9a07f5a51fb3d8
jeferson.leao <jeferson.leao@ideais.com.br>
2014-07-09 10:30:29 -0300

    add some keybindings

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var ioloop = require('./lib/ioloop');
 
 
-ioloop.addBinding(function(key, session){
-  if( key && key.name === 'up'){
-    console.log('pressed up key');
-  }
-});
-
+ioloop.setPrefix('nsh');
 ioloop.start();
