a072cad727debed96286679eae938cf42ffae229
jeferson.leao <jeferson.leao@ideais.com.br>
2014-07-11 10:01:14 -0300

    resonably stable

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', function(answer) {
+  console.log('Hello,', answer);
+  rl.close();
+});
